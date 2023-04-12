36eba7caaf196b65af0fb846537b9e90fa372af7
carlLGN <125569111+carlLGN@users.noreply.github.com>
2023-04-12 15:26:06 +0200

    Added changes

--- load_data.py
+++ load_data.py
@@ -2,8 +2,15 @@ import networkx as nx
 
 def load_data(mode="paper2paper"):
     if mode=="paper2paper":
-        return nx.read_gml("./paper2paper_2000_gcc.gml")
+        return nx.read_gml("./Data/paper2paper_2000_gcc.gml")
     
     if mode=="author2paper":
-        return nx.read_gml("./author2paper_2000_gcc.gml")
+        return nx.read_gml("./Data/author2paper_2000_gcc.gml")
 
+
+graph = load_data()
+
+
+print(graph)
+
+print('123')
