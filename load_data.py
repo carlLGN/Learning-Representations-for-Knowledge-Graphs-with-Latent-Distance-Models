94522fa760286d2c0958797db8c7580428fd3a6f
maltheaugust <s214631@dtu.dk>
2023-04-12 14:50:46 +0200

    Bugfixes

--- load_data.py
+++ load_data.py
@@ -4,6 +4,6 @@ def load_data(mode="paper2paper"):
     if mode=="paper2paper":
         return nx.read_gml("./paper2paper_2000_gcc.gml")
     
-    if mode=="author2paper"
+    if mode=="author2paper":
         return nx.read_gml("./author2paper_2000_gcc.gml")
 
