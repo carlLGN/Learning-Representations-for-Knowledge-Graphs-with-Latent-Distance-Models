303885151fcd8a6f21d16beec533955283b22878
carlLGN <125569111+carlLGN@users.noreply.github.com>
2023-04-12 15:41:17 +0200

    Made create and load subgraph

--- load_data.py
+++ load_data.py
@@ -1,16 +1,17 @@
 import networkx as nx
 
 def load_data(mode="paper2paper"):
-    if mode=="paper2paper":
-        return nx.read_gml("./Data/paper2paper_2000_gcc.gml")
+    return nx.read_gml(f"./Data/{mode}_2000_gcc.gml")
     
-    if mode=="author2paper":
-        return nx.read_gml("./Data/author2paper_2000_gcc.gml")
+def load_subgraph(mode = "paper2paper"):
+    return nx.read_gml(f"./Data/subgraph_{mode}.gml")
+    
 
+def create_subgraph(mode = "paper2paper", nodes=1000):
+    graph = load_data(mode)
+    node_subset = {i for i in range(nodes)}
+    subgraph = graph.subgraph(node_subset)
+    
+    nx.write_gml(subgraph, f"./Data/subgraph_{mode}.gml")
 
-graph = load_data()
-
-
-print(graph)
-
-print('123')
+    
