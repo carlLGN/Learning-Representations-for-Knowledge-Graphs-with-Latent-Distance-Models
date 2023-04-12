797dbf68a18ade51e30f6440c6ad28e4378e1402
maltheaugust <s214631@dtu.dk>
2023-04-12 16:38:06 +0200

    Bugfixes

--- load_data.py
+++ load_data.py
@@ -9,9 +9,9 @@ def load_subgraph(mode = "paper2paper"):
 
 def create_subgraph(mode = "paper2paper", nodes=1000):
     graph = load_data(mode)
-    node_subset = {i for i in range(nodes)}
-    subgraph = graph.subgraph(node_subset)
-    
+    print(graph)
+    subgraph = graph.subgraph(list(graph.nodes)[0:nodes])
+    print(subgraph)
     nx.write_gml(subgraph, f"./Data/subgraph_{mode}.gml")
 
     
