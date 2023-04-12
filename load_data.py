b029ab4b5e07cd4b7a77c64e183c29b81578001c
SaraAndersen <s202186@student.dtu.dk>
2023-04-12 14:37:38 +0200

    Data loading function

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+def load_data(mode="paper2paper"):
+    if mode=="paper2paper":
+        return nx.read_gml("./paper2paper_2000_gcc.gml")
+    
+    if mode=="author2paper"
+        return nx.read_gml("./author2paper_2000_gcc.gml")
+
