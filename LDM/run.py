c72e9305e86da8454cf5dcb06911c3b341b644e0
carlLGN <125569111+carlLGN@users.noreply.github.com>
2023-06-02 12:51:38 +0200

    Added a process debug option

--- LDM/run.py
+++ LDM/run.py
@@ -79,9 +79,43 @@ def process(args):
     if args.visualize:
         # Visualize the embeddings
         utils.visualize(ldm.get_embs().detach().cpu().numpy())
+        
+
+def process_debug(args):
+    dataset_path = args[0]
+    emb_path = args[1]
+    dim = args[2]
+    epoch_num = args[3]
+    steps_per_epoch = args[4]
+    batch_size = args[5]
+    lr = args[6]
+
+    seed = args[7]
+    verbose = args[8]
+    
+    edges = utils.read_emb(dataset_path)
+    edges = torch.as_tensor(edges, dtype=torch.int, device=torch.device("cpu")).T
+
+    # Run the model
+    ldm = LDM(
+        edges=edges, dim=dim, lr=lr, epoch_num=epoch_num, batch_size=batch_size, spe=steps_per_epoch,
+        device=torch.device(device), verbose=verbose, seed=seed
+    )
+    # Learn the embeddings
+    ldm.learn()
+    # Save the model
+    ldm.save_embs(path=emb_path)
+
+    if args.visualize:
+        # Visualize the embeddings
+        utils.visualize(ldm.get_embs().detach().cpu().numpy())
+    
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
+    #args = parse_arguments()
+    #process(args)
+    
+    args = ['./Data/paper2paper_edgelist', './ldm_paper2papertest.emb', 2, 200, 10, 50, 0.1, 19, 1, 1]
 
-    process(args)
+    process_debug(args)
