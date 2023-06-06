769857ee3178bcb5038317736ceb8e617b0fb41b
JacobNielsen1 <s204093@dtu.dk>
2023-06-06 11:34:01 +0200

    updates

--- Network_in_vectorspace.py
+++ Network_in_vectorspace.py
@@ -3,7 +3,7 @@ from load_data import load_subgraph
 import numpy as np
 import matplotlib.pyplot as plt
 
-ldm_paper2paper = np.asarray(read_emb('./Data/ldm_paper2papertest.emb'))
+ldm_paper2paper = np.asarray(read_emb('./Embeddings/a_init.emb'))
 
 def read_edges(path):
     edges=[]
@@ -32,25 +32,22 @@ def paper2paper_in_network(path):
 
 
 
-def paper2paper_in_network_specific_points(path): 
+def paper2paper_in_network_specific_points(path, nodes_looked_at): 
 
     edges=read_edges(path)
 
     xval=edges[:,0]
     yval=edges[:,1]
 
-    #just input the nodes of interest
-    nodes_looked_at=[6, 54, 388]
-
-
-    plt.plot(ldm_paper2paper[:,0], ldm_paper2paper[:,1], 'o')
+    plt.scatter(ldm_paper2paper[:,0], ldm_paper2paper[:,1], alpha=0.1)
 
     for i in range(len(xval)):
         if xval[i] in nodes_looked_at or yval[i] in nodes_looked_at:
             xvalues=ldm_paper2paper[int(xval[i])]
             yvalues=ldm_paper2paper[int(yval[i])]
 
-            plt.plot(xvalues,yvalues)
+            plt.plot(xvalues,yvalues, color='red', alpha=0.6)
+            plt.scatter(xvalues, yvalues, alpha=0.6, color='red')
 
         else:
             pass
@@ -60,7 +57,9 @@ def paper2paper_in_network_specific_points(path):
 
 
 if __name__ == '__main__':
-    paper2paper_in_network_specific_points('./Data/paper2paper_edgelist')
+    paper2paper_in_network_specific_points('./Data/paper2paper_edgelist', [6, 54, 388])
+    #plt.scatter(ldm_paper2paper[:,0], ldm_paper2paper[:,1])
+    #plt.show()
 
 
 
