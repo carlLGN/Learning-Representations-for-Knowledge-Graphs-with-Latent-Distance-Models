c31ebd4d683dc56d389f90f512d05f87af88b8db
carlLGN <125569111+carlLGN@users.noreply.github.com>
2023-06-08 14:49:46 +0200

    Updated run to work with multimodal

--- LDM/run.py
+++ LDM/run.py
@@ -1,6 +1,7 @@
 import torch
 from argparse import ArgumentParser, RawTextHelpFormatter
 from src.ldm import LDM
+from src.multimodal_ldm import Multimodal_LDM
 from src import utils
 
 # Global control for device
@@ -82,23 +83,28 @@ def process(args):
         
 
 def process_debug(args):
-    dataset_path = args[0]
-    emb_path = args[1]
-    dim = args[2]
-    epoch_num = args[3]
-    steps_per_epoch = args[4]
-    batch_size = args[5]
-    lr = args[6]
+    dataset_pp_path = args[0]
+    dataset_ap_path = args[1]
+    emb_path = args[2]
+    dim = args[3]
+    epoch_num = args[4]
+    steps_per_epoch = args[5]
+    alpha = args[6]
+    batch_size = args[7]
+    lr = args[8]
 
-    seed = args[7]
-    verbose = args[8]
+    seed = args[9]
+    verbose = args[10]
     
-    edges = utils.read_emb(dataset_path)
-    edges = torch.as_tensor(edges, dtype=torch.int, device=torch.device("cpu")).T
+    edges_pp = utils.read_emb(dataset_pp_path)
+    edges_pp = torch.as_tensor(edges_pp, dtype=torch.int, device=torch.device("cpu")).T
+
+    edges_ap = utils.read_emb(dataset_ap_path)
+    edges_ap = torch.as_tensor(edges_ap, dtype=torch.int, device=torch.device("cpu")).T
 
     # Run the model
-    ldm = LDM(
-        edges=edges, dim=dim, lr=lr, epoch_num=epoch_num, batch_size=batch_size, spe=steps_per_epoch,
+    ldm = Multimodal_LDM(
+        edges_pp=edges_pp, edges_ap=edges_ap, dim=dim, lr=lr, epoch_num=epoch_num, batch_size=batch_size, spe=steps_per_epoch, alpha=alpha,
         device=torch.device(device), verbose=verbose, seed=seed
     )
     # Learn the embeddings
@@ -116,6 +122,6 @@ if __name__ == "__main__":
     #args = parse_arguments()
     #process(args)
     
-    args = ['./Data/paper2paper_edgelist', './ldm_paper2papertest.emb', 2, 200, 10, 50, 0.1, 19, 1, 1]
+    args = ['./Data/paper2paper_edgelist', './Data/author2paper_edgelist', './ldm_paper2papertest.emb', 2, 200, 10, 0.5, 50, 0.1, 19, 1]
 
     process_debug(args)
