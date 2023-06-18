67263eeb993aec58a1003ac928b164f7d0a3d81e
carlLGN <125569111+carlLGN@users.noreply.github.com>
2023-06-18 14:03:18 +0200

    Changed LDM

--- LDM/src/ldm.py
+++ LDM/src/ldm.py
@@ -24,13 +24,20 @@ class LDM(torch.nn.Module):
         self.__set_seed(self.__seed)
 
         # Initialize the parameters
-        self.__beta = torch.nn.Parameter(
+        self.beta_p = torch.nn.Parameter(
             2 * torch.rand(size=(self.__nodes_num,), device=self.__device) - 1, requires_grad=True
         )
-        self.__z = torch.nn.Parameter(
+        
+        self.beta_p_star = torch.nn.Parameter(
+            2 * torch.rand(size=(self.__nodes_num,), device=self.__device) - 1, requires_grad=True
+        )
+        
+        self.p = torch.nn.Parameter(
             2 * torch.rand(size=(self.__nodes_num, self.__dim), device=self.__device) - 1, requires_grad=True
         )
-
+        self.p_star = torch.nn.Parameter(
+            2 * torch.rand(size=(self.__nodes_num, self.__dim), device=self.__device) - 1, requires_grad=True
+        )
         self.__epoch_num = epoch_num
         self.__steps_per_epoch = spe
         self.__batch_size = batch_size if batch_size else self.__nodes_num
@@ -52,24 +59,27 @@ class LDM(torch.nn.Module):
 
         return self.__z
 
-    def get_intensity_sum(self, nodes=None):
+    def get_intensity_sum(self, nodes_p_star, nodes_p):
 
-        beta = self.__beta if nodes is None else torch.index_select(self.__beta, index=nodes, dim=0)
-        z = self.__z if nodes is None else torch.index_select(self.__z, index=nodes, dim=0)
+        beta_p_star = self.beta_p_star if nodes_p_star is None else torch.index_select(self.beta_p_star, index=nodes_p_star, dim=0)
+        beta_p = self.beta_p if nodes_p is None else torch.index_select(self.beta_p, index=nodes_p, dim=0)
+        
+        p_star = self.p_star if nodes_p_star is None else torch.index_select(self.p_star, index=nodes_p_star, dim=0)
+        p = self.p_star if nodes_p_star is None else torch.index_select(self.p, index=nodes_p, dim=0)
+        
+        beta_mat = beta_p_star.unsqueeze(0) + beta_p.unsqueeze(1)
+        dist_mat = torch.cdist(p_star, p, p=2)
 
-        beta_mat = beta.unsqueeze(0) + beta.unsqueeze(1)
-        dist_mat = torch.cdist(z, z, p=2)
-
-        return torch.triu(torch.exp(beta_mat - dist_mat), diagonal=1).sum()
+        return torch.triu(torch.exp(beta_mat.T - dist_mat), diagonal=1).sum()
 
     def get_log_intensity_sum(self, edges):
 
-        beta_pair = torch.index_select(self.__beta, index=edges[0], dim=0) + \
-                    torch.index_select(self.__beta, index=edges[1], dim=0)
+        beta_pair = torch.index_select(self.beta_p_star, index=edges[0], dim=0) + \
+                    torch.index_select(self.beta_p, index=edges[1], dim=0)
 
         z_dist = self.__pdist(
-            torch.index_select(self.__z, index=edges[0], dim=0),
-            torch.index_select(self.__z, index=edges[1], dim=0),
+            torch.index_select(self.p, index=edges[0], dim=0),
+            torch.index_select(self.p_star, index=edges[1], dim=0),
         )
 
         return (beta_pair - z_dist).sum()
@@ -81,13 +91,13 @@ class LDM(torch.nn.Module):
         return torch.exp(beta_sum - z_dist)
 
 
-    def get_neg_likelihood(self, edges, nodes=None):
+    def get_neg_likelihood(self, edges, nodes_p_star=None, nodes_p=None):
 
         # Compute the link term
         link_term = self.get_log_intensity_sum(edges=edges)
 
         # Compute the non-link term
-        non_link = self.get_intensity_sum(nodes=nodes)
+        non_link = self.get_intensity_sum(nodes_p_star=nodes_p_star, nodes_p=nodes_p)
 
         return -(link_term - non_link)
 
@@ -136,8 +146,9 @@ class LDM(torch.nn.Module):
         self.train()
 
         sampled_nodes = torch.multinomial(self.__sampling_weights, self.__batch_size, replacement=False)
-        sampled_nodes, _ = torch.sort(sampled_nodes, dim=0)
+        sampled_p_star_nodes, _ = torch.sort(sampled_nodes, dim=0)
 
+        '''
         batch_edges, _ = spspmm(
             indexA=self.__edges.type(torch.long),
             valueA=torch.ones(size=(self.__edges_num, ), dtype=torch.float, device=self.__device),
@@ -145,15 +156,27 @@ class LDM(torch.nn.Module):
             valueB=torch.ones(size=(self.__batch_size,), dtype=torch.float, device=self.__device),
             m=self.__nodes_num, k=self.__nodes_num, n=self.__nodes_num, coalesced=True
         )
+        '''
+        #Author paper graph edge sampling
+        #Edges 
+        nodes_num = self.__nodes_num
+        edges_num = self.__edges_num
+        A = torch.sparse_coo_tensor(indices=self.__edges.to(torch.long), values=torch.ones(edges_num, ),
+        size=(nodes_num, nodes_num)).coalesce()
+        # Select the edges among the batch nodes
+        batch_idx = torch.index_select(A, dim=0, index=sampled_nodes).coalesce().indices()
+        # Get the batch edges
+        batch_edges = torch.vstack((sampled_p_star_nodes[batch_idx[0]], batch_idx[1]))
+        sampled_p_nodes = torch.tensor(list(set(batch_edges[1].tolist())))
 
         # Forward pass
-        average_batch_loss = self.forward(edges=batch_edges, nodes=sampled_nodes)
+        average_batch_loss = self.forward(edges=batch_edges, nodes_p_star=sampled_p_star_nodes, nodes_p = sampled_p_nodes)
 
         return average_batch_loss
 
-    def forward(self, edges, nodes):
+    def forward(self, edges, nodes_p_star, nodes_p):
 
-        nll = self.get_neg_likelihood(edges=edges, nodes=nodes)
+        nll = self.get_neg_likelihood(edges=edges, nodes_p_star = nodes_p_star, nodes_p=nodes_p)
 
         return nll
 
